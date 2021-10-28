63e66e3278cfa4caf28d914a1fce20d1258a026c
Gastón González <tongagonzalez22@gmail.com>
2021-10-28 10:46:27 -0300

    Actualizacion dinamica numero de upvotes

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,27 +3,27 @@ import React, {useEffect, useState} from "react";
 import Footer from "../components/footer";
 import AddNewPost from "../components/add-new-post";
 import Post from "../components/post";
-import {collection, getDocs, orderBy, query} from "firebase/firestore";
+import {collection, orderBy, query, onSnapshot} from "firebase/firestore";
 import {db} from "../lib/firebase";
-import {Stack} from "@mui/material";
+import {Link, Stack} from "@mui/material";
 
 export default function Main() {
 	const [posts, setPosts] = useState([]);
 
-	//TODO: Ver como obtener los datos y pasarlos a los demás componentes
-
 	useEffect(  async () => {
 		const q = query(collection(db, "posts"), orderBy("createdAt", "desc"));
-		const querySnapshot = await getDocs(q);
-		const _posts = [];
-		querySnapshot.forEach((doc) => {
-			_posts.push({
-				id: doc.id,
-				...doc.data(),
-			})
-		})
-		setPosts(_posts);
-	},[])
+		onSnapshot(q,(querySnapshot) => {
+			const _posts = [];
+			querySnapshot.forEach((doc) => {
+				_posts.push({
+					id: doc.id,
+					...doc.data(),
+				});
+
+			});
+			setPosts(_posts);
+		});
+	},[]);
 
 	return (
 		<>
