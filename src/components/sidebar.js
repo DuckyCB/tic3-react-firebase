f6ffe91dd673a0a76478764bcfeffb419e167a22
maguerre1 <maguerre1@correo.um.edu.uy>
2021-11-04 12:26:05 -0300

    Sidebar megapete

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,28 @@
+import {Container, Stack} from "@mui/material";
+import * as PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import Paper from "@mui/material/Paper";
+import ChildCareIcon from '@mui/icons-material/ChildCare';
+import {blue, orange} from "@mui/material/colors";
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: 'center',
+	color: theme.palette.text.secondary,
+	square: true,
+	width: theme.spacing(25)
+}));
+
 export default function Sidebar() {
 	return (
-		<div>
-			Sidebar
-		</div>
+		<Container fixed>
+				Top Kinchoos
+			<Stack spacing={1} >
+				<Item>Quincke </Item>
+				<Item>Item 2</Item>
+				<Item>Item 3</Item>
+			</Stack>
+		</Container>
 	)
 }
