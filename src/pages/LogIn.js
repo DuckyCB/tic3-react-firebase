357846b7dba5ca16f2fe03f4abf90ebe44154ece
DuckyCB <patrickcarrau@gmail.com>
2021-11-17 22:38:07 -0300

    A

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -17,48 +17,6 @@ import Typography from '@mui/material/Typography';
 import { loginUser } from "../utils/userUtils";
 import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
 
-// default function AlertDialog() {
-// 	const [open, setOpen] = React.useState(false);
-//
-// 	const handleClickOpen = () => {
-// 		setOpen(true);
-// 	};
-//
-// 	const handleClose = () => {
-// 		setOpen(false);
-// 	};
-//
-// 	return (
-// 		<div>
-// 			<Button variant="outlined" onClick={handleClickOpen}>
-// 				Open alert dialog
-// 			</Button>
-// 			<Dialog
-// 				open={open}
-// 				onClose={handleClose}
-// 				aria-labelledby="alert-dialog-title"
-// 				aria-describedby="alert-dialog-description"
-// 			>
-// 				<DialogTitle id="alert-dialog-title">
-// 					{"Use Google's location service?"}
-// 				</DialogTitle>
-// 				<DialogContent>
-// 					<DialogContentText id="alert-dialog-description">
-// 						Let Google help apps determine location. This means sending anonymous
-// 						location data to Google, even when no apps are running.
-// 					</DialogContentText>
-// 				</DialogContent>
-// 				<DialogActions>
-// 					<Button onClick={handleClose}>Disagree</Button>
-// 					<Button onClick={handleClose} autoFocus>
-// 						Agree
-// 					</Button>
-// 				</DialogActions>
-// 			</Dialog>
-// 		</div>
-// 	);
-// }
-
 
 export default function LogIn() {
 	const history = useHistory();
